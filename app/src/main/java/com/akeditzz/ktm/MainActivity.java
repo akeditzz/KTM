d9cc1156fc628a6ec61800272e44900254a521fb
Akshay <akshaymanagooli@yahoo.com>
2018-06-09 23:30:25 +0530

    MAPS INTENT ADDED

--- app/src/main/java/com/akeditzz/ktm/MainActivity.java
+++ app/src/main/java/com/akeditzz/ktm/MainActivity.java
@@ -1,13 +1,26 @@
 package com.akeditzz.ktm;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_address:
+                startActivity(new Intent(Intent.ACTION_VIEW,Uri.parse(getString(R.string.uri_address))));
+                break;
+        }
+
     }
 }
